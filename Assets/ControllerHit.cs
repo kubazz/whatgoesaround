7c2518552b5f707c61b686f6b2f333284b94cc0a
Jakub Ziembinski <kubazz@gmail.com>
2015-05-16 01:50:45 +0200

    player controler

---
+++ Assets/ControllerHit.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerHit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		//Debug.Log("hit");
+		if (collision.collider.gameObject.layer == LayerMask.NameToLayer ("Obstacles")) {
+			Debug.Log("hit2");
+			//Application.LoadLevel(Application.loadedLevel);
+		}
+		
+	}
+
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+
+		   
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+
+	public int lane = 2;
+	public Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+		//startPos = 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			lane--;
+		}
+		if (Input.GetKeyDown (KeyCode.D)) {
+			lane++;
+		}
+
+		float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+		var pos = transform.position;
+		pos.z += horizontal;
+
+		pos.z = Mathf.Clamp(pos.z, 3.0f, 33.0f);
+		transform.position = pos;
+	}
+}
